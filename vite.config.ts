d0b535295b80fbf1db908464b278924a69ac75bf
Muhammad7658 <exposite285@gmail.com>
2025-07-14 17:59:20 +0500

    deplyoing to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 ADD THIS LINE with your actual repo name
+
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
